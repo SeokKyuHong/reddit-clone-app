286bf31bac2873482e716a6cb66b0d6a9179b50d
SeokKyuHong <hongsk5689@gmail.com>
2023-02-27 14:21:37 +0900

    Entity완료

---
+++ server/src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "password",
+    database: "postgres", 
+    synchronize: true,  //개발 환경에서만 true로 해야함
+    logging: false,
+    entities: ["src/entities/**/*.ts"],
+    migrations: [],
+    subscribers: [],
+})
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import { AppDataSource } from "./data-source"
 
 const app = express();
 
@@ -13,4 +14,8 @@ let port = 4000;
 
 app.listen(port, async () => {
     console.log(`Server running at http://localhost:${port}`);
+
+    AppDataSource.initialize().then(async () => {
+        console.log(`database initialized`)
+    }).catch(error => console.log(error))
 });
